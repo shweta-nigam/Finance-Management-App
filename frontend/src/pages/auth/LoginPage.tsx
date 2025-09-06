24be176ce1c6508255f3cdb2ce5f28f22b610d97
Shweta Nigam <shwetanigam2106@gmail.com>
2025-09-06 16:15:23 +0530

    fix(LoginPage) : add space

---
+++ frontend/src/pages/auth/LoginPage.tsx
@@ -0,0 +1,97 @@
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+
+import { useLogin } from "../../hooks/useAuth.ts";
+import { useState } from "react";
+
+export function LoginPage() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const [login, response, error, loading] = useLogin();
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault(); // stop page refresh
+    login("/api/v1/auth/login", { email, password });
+  };
+
+  if (error) {
+    return <p>Something went wrong</p>;
+  }
+
+  return (
+    <Card className="w-full max-w-sm">
+      <CardHeader>
+        <CardTitle>Login to your account</CardTitle>
+        <CardDescription>
+          <p>
+            First time here?
+            <a
+              className="ml-2 inline-block text-sm underline-offset-4 hover:underline text-blue-800 "
+              href="http://localhost:5173/signup"
+            >
+              Signup
+            </a>
+          </p>
+        </CardDescription>
+      </CardHeader>
+      <CardContent>
+        <form onSubmit={handleSubmit}>
+          <div className="flex flex-col gap-6">
+            <div className="grid gap-2 text-left">
+              <Label htmlFor="email">Email</Label>
+              <Input
+                id="email"
+                type="email"
+                placeholder="m@example.com"
+                required
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+            </div>
+            <div className="grid gap-2">
+              <div className="flex items-center">
+                <Label htmlFor="password">Password</Label>
+                <a
+                  href="#"
+                  className="ml-auto inline-block text-sm underline-offset-4 hover:underline"
+                >
+                  Forgot your password?
+                </a>
+              </div>
+              <Input
+                id="password"
+                type="password"
+                required
+                value={password}
+                onChange={(e) => {
+                  console.log("typed", e.target.value);
+                  setPassword(e.target.value);
+                }}
+              />
+            </div>
+          </div>
+          {error && <p className="text-red-500"> Error: {error}</p>}
+          {response && <p className="text-green-600"> Login successful</p>}
+        </form>
+      </CardContent>
+      <CardFooter className="flex-col gap-2">
+        <Button type="submit" className="w-full" onClick={handleSubmit}>
+          {loading ? "Logging in..." : "Login"}
+        </Button>
+        <Button variant="outline" className="w-full">
+          Login with Google
+        </Button>
+      </CardFooter>
+    </Card>
+  );
+}

305ea5d14b0e5c588581a053ea4981e07052a334
Shweta Nigam <shwetanigam2106@gmail.com>
2025-09-07 19:13:54 +0530

    fix: import navbar in App.tsx file

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,13 @@
 import { BrowserRouter } from 'react-router-dom'
 import './App.css'
 import { AppRoutes } from './routes'
+import { Navbar } from './components/Navbar'
 
 function App() {
 
   return (
     <BrowserRouter>
+     <Navbar />
      <AppRoutes />
     </BrowserRouter>
   )

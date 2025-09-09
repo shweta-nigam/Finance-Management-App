504263270075632509576717cd6a2530a3f3e134
Shweta Nigam <shwetanigam2106@gmail.com>
2025-09-09 20:36:40 +0530

    feat: add budget route

--- frontend/src/routes/dashboardRoutes.tsx
+++ frontend/src/routes/dashboardRoutes.tsx
@@ -1,12 +1,14 @@
 import { Route } from "react-router-dom";
 import Profile from "@/pages/dashboard/Profile";
 import Dashboard from "@/pages/dashboard/DashBoard";
+import BudgetPage from "@/pages/dashboard/Budget";
 
 export default function DashboardRoutes() {
   return (
     <>
       <Route path="/dashboard" element={<Dashboard />}>  {/*Absolute path */}
         <Route path="profile" element={<Profile />} />      {/*relative path */}
+        <Route path="budgets" element={<BudgetPage />} />   
       </Route>
     </>
   );

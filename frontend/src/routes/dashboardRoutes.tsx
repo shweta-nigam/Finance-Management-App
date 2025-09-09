1e6a816bcb5a3f2a66c0ea4da472310c453b4330
Shweta Nigam <shwetanigam2106@gmail.com>
2025-09-09 22:26:03 +0530

    fix(routing): set Profile as default route in Dashboard

--- frontend/src/routes/dashboardRoutes.tsx
+++ frontend/src/routes/dashboardRoutes.tsx
@@ -6,9 +6,10 @@ import BudgetPage from "@/pages/dashboard/Budget";
 export default function DashboardRoutes() {
   return (
     <>
-      <Route path="/dashboard" element={<Dashboard />}>  {/*Absolute path */}
-        <Route path="profile" element={<Profile />} />      {/*relative path */}
-        <Route path="budgets" element={<BudgetPage />} />   
+      <Route path="/dashboard" element={<Dashboard />}> {/*Absolute path */}
+        <Route index element={<Profile />} />
+        <Route path="profile" element={<Profile />} /> {/*relative path */}
+        <Route path="budgets" element={<BudgetPage />} />
       </Route>
     </>
   );

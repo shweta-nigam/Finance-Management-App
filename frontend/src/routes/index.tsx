12e3736c9f403bf1158c6cf0050339e2da722b97
Shweta Nigam <shwetanigam2106@gmail.com>
2025-09-10 15:20:56 +0530

    feat: add about route

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -1,14 +1,16 @@
-import { Routes, Route, Navigate } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import { AuthRoute } from "./authRoutes";
 import HomePage from "@/pages/HomePage";
 import DashboardRoutes from "./dashboardRoutes";
 import NotFoundPage from "@/pages/NotFoundPage";
+import AboutPage from "@/pages/AboutPage";
 
 export function AppRoutes() {
   return (
     <>
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="/about" element={<AboutPage />} />
         {AuthRoute()}
         {DashboardRoutes()}
         <Route path="*" element={<NotFoundPage />} />

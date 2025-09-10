ab536017a8e942fdfa0bade5277d11aa9eeec8df
Shweta Nigam <shwetanigam2106@gmail.com>
2025-09-10 23:43:27 +0530

    feat: add price route

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -5,6 +5,7 @@ import DashboardRoutes from "./dashboardRoutes";
 import NotFoundPage from "@/pages/NotFoundPage";
 import AboutPage from "@/pages/AboutPage";
 import ContactPage from "@/pages/ContactPage";
+import PricePage from "@/pages/PricePage";
 
 export function AppRoutes() {
   return (
@@ -13,6 +14,7 @@ export function AppRoutes() {
         <Route path="/" element={<HomePage />} />
         <Route path="/about" element={<AboutPage />} />
         <Route path="/contact" element={<ContactPage />} />
+        <Route path="/price" element={<PricePage />} />
         {AuthRoute()}
         {DashboardRoutes()}
         <Route path="*" element={<NotFoundPage />} />

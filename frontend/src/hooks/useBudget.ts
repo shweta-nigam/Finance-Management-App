6f2961fa5f0d274f54e6f1937176e907e71e01e7
Shweta Nigam <shwetanigam2106@gmail.com>
2025-09-13 11:40:42 +0530

    feat(useBudget): add chart data

--- frontend/src/hooks/useBudget.ts
+++ frontend/src/hooks/useBudget.ts
@@ -1,68 +1,88 @@
 import axios from "axios"
 import { useState } from "react"
 
-export function useBudget(){
-  const [response, setResponse] = useState<any>(null)
-  const [error, setError] = useState<null | string>(null)
+export function useBudget() {
+    const [response, setResponse] = useState<any>(null)
+    const [error, setError] = useState<null | string>(null)
+    const [chartData, setChartData] = useState<any[]>([])
 
 
-    const createBudget = async(urlPath:string, data:any) => {
+
+    const createBudget = async (urlPath: string, data: any) => {
         try {
             setError(null)
-            const res = await axios.post(urlPath,data)
+            const res = await axios.post(urlPath, data)
             setResponse(res.data.data.budget)
-        } catch (error:any) {
+        } catch (error: any) {
             setError(error.response?.data?.message || "Something went wrong while creating budget")
         }
     }
-    const updateBudget = async(urlPath:string, data:any) => {
+    const updateBudget = async (urlPath: string, data: any) => {
         try {
             setError(null)
-            const res = await axios.patch(urlPath,data)
+            const res = await axios.patch(urlPath, data)
             setResponse(res.data.data.budget)
-        } catch (error:any) {
+        } catch (error: any) {
             setError(error.response?.data?.message || "Something went wrong while updating budget")
         }
     }
-    const getBudget = async(urlPath:string) => {
+    const getBudget = async (urlPath: string) => {
         try {
             setError(null)
             const res = await axios.get(urlPath)
             setResponse(res.data.data)
-        } catch (error:any) {
+
+            // tranform to chart data
+            if (res.data.data?.date && res.data.data?.amount) {
+                setChartData([
+                    {
+                        day: new Date(res.data.data.date).getDate(),
+                        amount: res.data.data.amount,
+                    }
+                ])
+            }
+
+        } catch (error: any) {
             setError(error.response?.data?.message || "Something went wrong while fetching budget details")
         }
     }
-    const getAllBudgets = async(urlPath:string) => {
+    const getAllBudgets = async (urlPath: string) => {
         try {
             setError(null)
             const res = await axios.get(urlPath)
             setResponse(res.data.data.budgets)
-        } catch (error:any) {
-            setError(error.response?.data?.message  || "Something went wrong while fetching budgets details")
+
+            // convert to chart data
+            const tranformed = res.data.data.budgets.map((b: any) => ({
+                day: new Date(b.date).getDate(),
+                amount: b.amount,
+            }))
+            setChartData(tranformed)
+        } catch (error: any) {
+            setError(error.response?.data?.message || "Something went wrong while fetching budgets details")
         }
     }
-    const deleteBudget = async(urlPath:string) => {
+    const deleteBudget = async (urlPath: string) => {
         try {
             setError(null)
             const res = await axios.delete(urlPath)
             setResponse(res.data.data.budget)
-        } catch (error:any) {
+        } catch (error: any) {
             setError(error.response?.data?.message || "Something went wrong while deleting budget")
         }
     }
-    const deleteAllBudgets = async(urlPath:string) => {
+    const deleteAllBudgets = async (urlPath: string) => {
         try {
             setError(null)
             const res = await axios.delete(urlPath)
             setResponse(res.data.data)                    // as data is null in backend
             // setResponse(res.data.message)     // to show a msg
-        } catch (error:any) {
+        } catch (error: any) {
             setError(error.response?.data?.message || "Something went wrong while deleting budgets")
         }
     }
 
-    return {response, error, createBudget, updateBudget, getBudget, getAllBudgets,deleteBudget, deleteAllBudgets}
+    return { response, chartData, error, createBudget, updateBudget, getBudget, getAllBudgets, deleteBudget, deleteAllBudgets }
 }
 
 //Note:-

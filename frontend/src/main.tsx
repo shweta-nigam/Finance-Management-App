019c04fe78cb5ac4b95392a1adb2f474cda70005
Shweta Nigam <shwetanigam2106@gmail.com>
2025-09-17 13:34:23 +0530

    feat: use wrapper of AuthProvider

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { AuthProvider } from "./context/AuthContext.tsx";
 // import { BrowserRouter } from "react-router-dom";
 
 const queryClient = new QueryClient();
@@ -10,7 +11,9 @@ const queryClient = new QueryClient();
 createRoot(document.getElementById("root")!).render(
   <StrictMode>                                                  
     <QueryClientProvider client={queryClient}>   
+    <AuthProvider>
         <App />
+        </AuthProvider>
     </QueryClientProvider>
   </StrictMode>
 );

efecd6dac50d94369e58eacdcc470818763c0ecb
Shweta Nigam <shwetanigam2106@gmail.com>
2025-09-25 13:47:09 +0530

    fix :fetch all expenses on mount and save directly to state, handle object as well array in setExpenses

--- frontend/src/context/ExpenseContext.tsx
+++ frontend/src/context/ExpenseContext.tsx
@@ -10,7 +10,7 @@ import type { Expense } from "@/types";
 
 type ExpenseContextType = {
   expenses: Expense[];
-  addExpense: (expense: Omit<Expense, "id">) => Promise<void>;
+  addExpense: (expense: Omit<Expense, "id">) => Promise<Expense>;
   clearExpense: () => void;
 };
 
@@ -19,18 +19,42 @@ export const ExpenseContext = createContext<ExpenseContextType | undefined>(
 );
 
 export function ExpenseProvider({ children }: { children: ReactNode }) {
+
   const [expenses, setExpenses] = useState<Expense[]>([]);
   const { response, getAllExpenses, createExpense } = useExpenseApi();
 
+  // fetch all expenses on mount and save directly to state
   useEffect(() => {
-    getAllExpenses("/api/v1/expense/");
+    (async() => {
+      try {
+        const all =  await getAllExpenses("/api/v1/expense/");
+        setExpenses(all)
+      } catch (error) {
+        console.error("Failed to fetch expenses:", error);
+      }
+    })()
+  
   }, []);
 
   useEffect(() => {
     if (response) {
-       console.log("Expense API response:(from expense context code file", response);
+      console.log(
+        "Expense API response:(from expense context code file",
+        response
+      );
       if (Array.isArray(response)) {
         setExpenses(response);
+      } else if (typeof response === "object") {
+        // add single object
+        setExpenses((prev) => {
+          const exists = prev.find((e) => e.id === (response as Expense).id);
+          if (exists) {
+            return prev.map((e) =>
+              e.id === (response as Expense).id ? (response as Expense) : e
+            );
+          }
+          return [response as Expense, ...prev];
+        });
       } else {
         console.warn("Expense API returned non-array response:", response);
         setExpenses([]);
@@ -40,11 +64,18 @@ export function ExpenseProvider({ children }: { children: ReactNode }) {
 
   const addExpense = async (newExpense: Omit<Expense, "id">) => {
     try {
+      console.log("addExpense called with:", newExpense);
+
       const saved = await createExpense("/api/v1/expense/", newExpense);
 
+      console.log("createExpense returned:", saved);
+
       setExpenses((prev) => [saved, ...prev]);
+
+      return saved;
     } catch (err) {
-      console.error("Failed to add expense:", err);
+      console.error("Failed to add expense(in provider)", err);
+      throw err;
     }
   };
 

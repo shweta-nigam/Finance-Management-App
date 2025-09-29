9805b98b605146bce3a33f86feb8ddfac13b4be9
Shweta Nigam <shwetanigam2106@gmail.com>
2025-09-29 12:35:02 +0530

    feat(budgetContext): integrate api call + context in single file

--- frontend/src/context/BudgetContext.tsx
+++ frontend/src/context/BudgetContext.tsx
@@ -1,10 +1,6 @@
-import { createContext, useContext, useState, type ReactNode } from "react";
-type Budget = {
-  id: string;
-  amount: number;
-  date: Date;
-  frequency: string[];
-};
+import { createContext, useContext, useEffect, useState, type ReactNode } from "react";
+import type { Budget } from "@/types";
+import { useBudgetApi } from "@/hooks/useBudgetApi";
 
 type BudgetContextType = {
   budget: Budget | null;
@@ -19,9 +15,41 @@ export const BudgetContext = createContext<BudgetContextType | undefined>(
 
 export function BudgetProvider({children}: {children: ReactNode }) {
   const [budget, setBudget] = useState<Budget | null>(null);
+const {response , getAllBudgets, getBudget,  createBudget} = useBudgetApi()
 
-const addBudget = (newBudget:Budget) => {
+useEffect(()=>{
+(async()=>{
+try {
+  const budget = await getBudget("/api/v1/budget/")
+} catch (error) {
+  console.error("Failed to fetch budget:", error);
+}
+})()
+}, [])
+
+useEffect(()=>{
+  if(!response) return;
+
+  if(response){
+    setBudget(response);
+  }else {
+   console.warn("Budget API returned unexpected response:", response);
+   
+    
+  }
+},[response])
+
+const addBudget = async (newBudget:Budget) => {
+  try {
+    const saved = await createBudget("/api/v1/budget", newBudget)
+
     setBudget(newBudget)
+
+    return saved
+  } catch (err) {
+    console.error("Failed to add budget(in provider)", err);
+      throw err;
+  }
 }
 
 const clearBudget = ()=>{

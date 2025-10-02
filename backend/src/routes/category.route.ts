cc61f44d874805ac8c1c9c9fc1980e984b4f6986
Shweta Nigam <shwetanigam2106@gmail.com>
2025-10-02 14:56:59 +0530

    fix(backend route): change route name

--- backend/src/routes/category.route.ts
+++ backend/src/routes/category.route.ts
@@ -1,6 +1,6 @@
 import express from "express"
 import { isLoggedIn } from "../middlewares/auth.middleware"
-import { createCategory, deleteAllCategory, deleteCategory, getAllCategories, getCategory, updateCategory } from "../controllers/category.controller"
+import { createCategory, deleteAllCategories, deleteCategory, getAllCategories, getCategory, updateCategory } from "../controllers/category.controller"
 
 const categoryRoute = express.Router()
 
@@ -9,6 +9,6 @@ categoryRoute.patch("/:categoryId", isLoggedIn, updateCategory)
 categoryRoute.get("/:categoryId", isLoggedIn, getCategory)
 categoryRoute.get("/", isLoggedIn, getAllCategories)
 categoryRoute.delete("/:categoryId", isLoggedIn, deleteCategory)
-categoryRoute.delete("/", isLoggedIn, deleteAllCategory)
+categoryRoute.delete("/", isLoggedIn, deleteAllCategories)
 
 export default categoryRoute

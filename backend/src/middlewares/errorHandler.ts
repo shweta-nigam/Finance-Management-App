08e0da4727e894bff9e0ac55d1bd135fecd49434
Shweta Nigam <shwetanigam2106@gmail.com>
2025-10-05 11:48:12 +0530

    feat(backend:errorHandler): show 400 error for zod validation

--- backend/src/middlewares/errorHandler.ts
+++ backend/src/middlewares/errorHandler.ts
@@ -1,29 +1,39 @@
 import { ApiError } from "../utils/apiError";
-import {Request, NextFunction, Response} from "express"
-
+import { Request, NextFunction, Response } from "express"
+import { ZodError } from "zod";
 
 //Express expects error-handling middleware to have 4 parameters (err, req, res, next) — that’s how it recognizes it as an error handler.
 export const errorHandler = (
-    err: Error | ApiError,
+    err: Error | ApiError | ZodError<any>,
     req: Request,
     res: Response,
     next: NextFunction
 ) => {
-console.error("Error caught by errorHandler:", err);
-    //if it is our custom ApiError ,use its details
-if(err instanceof ApiError){
-    return res.status(err.statusCode).json({
-          success: err.success,
-          message: err.message,
-          errors:err.errors,
-          data:err.data
-    })
-}
+    console.error("Error caught by errorHandler:", err);
 
-// if it is some other error , send generic error
+    //if it is our custom ApiError ,use its details
+    if (err instanceof ApiError) {
+        return res.status(err.statusCode).json({
+            success: err.success,
+            message: err.message,
+            errors: err.errors,
+            data: err.data
+        })
+    }
+
+    // Handle Zod validation errors
+    if (err instanceof ZodError) {
+        return res.status(400).json({
+            success: false,
+            message: "Validation failed",
+            errors: err.issues
+        })
+    }
+
+    // if it is some other error , send generic error
 
     return res.status(500).json({
-        success:false,
+        success: false,
         message: err.message || "Internal server Error"
     })
 }

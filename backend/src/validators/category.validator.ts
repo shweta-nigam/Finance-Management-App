dbca454bf085321e2f37a63bc6d83608e65bba8b
Shweta Nigam <shwetanigam2106@gmail.com>
2025-10-12 17:23:25 +0530

    feat(backend): update validator to include extra filed in category.validator

--- backend/src/validators/category.validator.ts
+++ backend/src/validators/category.validator.ts
@@ -4,11 +4,14 @@ export const categoryCreateSchema = z.object({
     title: z.string().min(1, "Title is required"),
     description: z.string().min(1, "Description is required"),
     note: z.string().optional(),
-    type: z.enum(["Income", "Expense"]),
+    type: z.enum(["Income", "Expense", "Transfer"]),
     icon: z.string().optional(),
     color: z.string().optional(),
     isDeleted: z.boolean().optional(),
-    isDefault: z.boolean().optional()
+    isDefault: z.boolean().optional(),
+    user: z.string().optional(),
+    budget: z.string().optional(),
+    expense: z.string().optional(),
 })
 
 export const categoryUpdateSchema = categoryCreateSchema.partial();

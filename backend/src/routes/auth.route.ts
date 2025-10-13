4a6345bf79cd5990ff7d744d0fbb53f35cefd65a
Shweta Nigam <shwetanigam2106@gmail.com>
2025-10-13 22:36:01 +0530

    fix: modify http methods (verbs)

--- backend/src/routes/auth.route.ts
+++ backend/src/routes/auth.route.ts
@@ -5,9 +5,9 @@ import { isLoggedIn } from "../middlewares/auth.middleware"
 const authRoutes = express.Router()
 
 authRoutes.post("/register", register)
-authRoutes.get("/verify/:token", verify)
+authRoutes.post("/verify/:token", verify)
 authRoutes.post("/login", login)
-authRoutes.get("/logout",isLoggedIn, logout)
+authRoutes.post("/logout",isLoggedIn, logout)
 
 authRoutes.get("/google",isLoggedIn, googleAuth)
 

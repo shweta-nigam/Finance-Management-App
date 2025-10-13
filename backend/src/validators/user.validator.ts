ede5fae2374cd8eabc27f72c02fa49c1c619a672
Shweta Nigam <shwetanigam2106@gmail.com>
2025-10-13 15:38:17 +0530

    fix: correct small typo in user.validator

--- backend/src/validators/user.validator.ts
+++ backend/src/validators/user.validator.ts
@@ -5,14 +5,14 @@ const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$
 export const registerUserSchema = z.object({
     name: z.string().min(2, "Name is required"),
     username: z.string().min(3, "Username must be at least 3 characters").optional(),
-    email: z.email("Invalid email formate"),
+    email: z.email("Invalid email format"),
     password: z.string().regex(passwordRegex, "Password must contain uppercase, lowercase, number, and special character"),
     role: z.enum(["User", "Admin"]).optional(),
     isPlanActive: z.boolean().optional(),
     avatar: z.preprocess(
         (val) => {
             if (typeof val !== "string") return undefined;
-            const clean = val.trim().toLowerCase();
+            const clean = val.trim();
 
             if (clean === "" || clean === "null" || clean === "undefined") {
                 return undefined;

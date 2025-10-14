63f5a7ae11c02b4eb7bfda26150c6268b20b2187
Shweta Nigam <shwetanigam2106@gmail.com>
2025-10-14 11:01:02 +0530

    feat: new route , verify

--- frontend/src/routes/authRoutes.tsx
+++ frontend/src/routes/authRoutes.tsx
@@ -1,5 +1,6 @@
 import { LoginPage } from "@/pages/auth/LoginPage";
 import { SignupPage } from "@/pages/auth/SignupPage";
+import VerifyPage from "@/pages/auth/VerifyPage";
 import { Route } from "react-router-dom";
 
 export function AuthRoute() {
@@ -8,7 +9,8 @@ export function AuthRoute() {
 
         <Route path="/login/*" element={<LoginPage />} />
         <Route path="/signup/*" element={<SignupPage />} />
-    
+        <Route path="/verify/*" element={<VerifyPage />} />
+        
     </>
   );
 }

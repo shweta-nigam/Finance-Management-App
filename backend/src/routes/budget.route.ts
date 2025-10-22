fdac3d3c5ec2e4fcea96c6b155b13561599ed739
Shweta Nigam <shwetanigam2106@gmail.com>
2025-10-22 13:05:17 +0530

    fix: change case of BudgetId to lowercase

--- backend/src/routes/budget.route.ts
+++ backend/src/routes/budget.route.ts
@@ -5,10 +5,10 @@ import { isLoggedIn } from "../middlewares/auth.middleware"
 const budgetRoutes = express.Router()
 
 budgetRoutes.post("/", isLoggedIn, createBudget)
-budgetRoutes.patch("/:BudgetId", isLoggedIn, updateBudget)
+budgetRoutes.patch("/:budgetId", isLoggedIn, updateBudget)
 budgetRoutes.get("/", isLoggedIn, getAllBudgets)
-budgetRoutes.get("/:BudgetId", isLoggedIn, getBudgetById)
-budgetRoutes.delete("/:BudgetId", isLoggedIn, deleteBudget)
+budgetRoutes.get("/:budgetId", isLoggedIn, getBudgetById)
+budgetRoutes.delete("/:budgetId", isLoggedIn, deleteBudget)
 budgetRoutes.delete("/", isLoggedIn, deleteALlBudgets)
 
 export default budgetRoutes

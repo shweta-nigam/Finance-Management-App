e07233674d45d08e144bceb103c42b68a4ba141f
Shweta Nigam <shwetanigam2106@gmail.com>
2025-10-23 23:05:38 +0530

    feat: add totalBalance form util

--- frontend/src/pages/dashboard/Profile.tsx
+++ frontend/src/pages/dashboard/Profile.tsx
@@ -1,87 +1,16 @@
-// import UserAvatar from "@/components/UserAvatar";
-// import useAuth from "@/context/AuthContext";
-// import { Edit, Lock, Shield } from "lucide-react";
-
-// export default function Profile() {
-//   const { user } = useAuth();
-
-//   if (!user) {
-//     return <p className="text-xl bg text-white">Please log in to view your profile</p>;
-//   }
-
-//   return (
-//     <div className="max-w-5xl mx-auto p-6 text-white">
-//       {/* Profile Header */}
-//       <div className="relative bg-D-blue rounded-2xl shadow-lg">
-//         <div className="flex flex-col md:flex-row items-center md:items-end gap-6 p-8">
-//           <UserAvatar size="w-25 h-25 text-lg" />
-//           <div className="flex-1 text-center md:text-left">
-//             <h2 className="text-3xl font-bold">{user.name} </h2>
-//             <p className="text-gray-200">{user.email}</p>
-//           </div>
-//           <button className="btn shadow-md mt-6 transition btn-L-blue btn-L-blue:hover">
-//             <Edit className="w-4 h-4 mr-2" /> Edit Profile
-//           </button>
-//         </div>
-//       </div>
-
-//       {/*Information Section */}
-//       <div className="bg-D-blue rounded-xl shadow-md mt-8 p-6">
-//         <h3 className="text-xl font-semibold mb-6  border-b pb-3">
-//           Personal Information
-//         </h3>
-//         <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
-//           <div>
-//             <p className="text-gray-500 font-medium">Full Name</p>
-//             <p className="text-lg font-semibold ">{user.name}</p>
-//           </div>
-//           <div>
-//             <p className="text-gray-500 font-medium">Email</p>
-//             <p className="text-lg font-semibold ">{user.email}</p>
-//           </div>
-//           <div>
-//             <p className="text-gray-500 font-medium">username</p>
-//             {/* <p className="text-lg font-semibold ">{user.username}</p> */}
-//           </div>
-//           <div>
-//             <p className="text-gray-500 font-medium">Account Balance</p>
-//             {/* <p className="text-lg font-bold text-green-600">{user.balance}</p> */}
-//           </div>
-//         </div>
-//       </div>
-
-//       {/* Security Settings Section */}
-//       <div className="bg-D-blue rounded-xl shadow-md mt-8 p-6 text-white">
-//         <h3 className="text-xl font-semibold mb-6 border-b pb-3">
-//           Security Settings
-//         </h3>
-//         <div className="space-y-4">
-//           <div className="flex justify-between items-center py-2">
-//             <div className="flex items-center gap-2">
-//               <Lock className="w-5 h-5 text-indigo-600" />
-//               <span>Password</span>
-//             </div>
-//             <button className="btn btn-sm btn-outline">Change</button>
-//           </div>
-//           <div className="flex justify-between items-center py-2">
-//             <div className="flex items-center gap-2 ">
-//               <Shield className="w-5 h-5 text-indigo-600" />
-//               <span>Two-Factor Authentication</span>
-//             </div>
-//             <button className="btn btn-sm btn-outline">Enable</button>
-//           </div>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// }
-
 import UserAvatar from "@/components/UserAvatar";
 import useAuth from "@/context/AuthContext";
+import useExpense from "@/context/ExpenseContext";
+import useIncome from "@/context/IncomeContext";
+import { getTotalBalance } from "@/utils/financeCalculations";
 import { Edit, Lock, Shield } from "lucide-react";
 
 export default function Profile() {
   const { user } = useAuth();
+const {incomes} = useIncome()
+const {expenses} = useExpense()
+
+  const totalBalance = getTotalBalance(incomes,expenses)
 
   if (!user) {
     return (
@@ -147,7 +76,7 @@ export default function Profile() {
             <p className="text-gray-400 text-sm uppercase tracking-wide">
               Account Balance
             </p>
-            <p className="text-lg font-bold text-green-500">₹0.00</p>
+            <p className="text-lg font-bold text-green-500">{totalBalance}</p>
           </div>
         </div>
       </div>

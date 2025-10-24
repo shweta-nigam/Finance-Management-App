2a0d3c38cd77264f2106a931d502df798125e897
Shweta Nigam <shwetanigam2106@gmail.com>
2025-10-24 22:52:33 +0530

    feat : use GoogleOAuthProvider wrapper in main.tsx file

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,6 +4,7 @@ import "./index.css";
 import App from "./App.tsx";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { AuthProvider } from "./context/AuthContext.tsx";
+import {GoogleOAuthProvider} from "@react-oauth/google"
 
 const queryClient = new QueryClient();
 
@@ -11,7 +12,9 @@ createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
       <AuthProvider>
+        <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}>
         <App />
+        </GoogleOAuthProvider>
       </AuthProvider>
     </QueryClientProvider>
   </StrictMode>

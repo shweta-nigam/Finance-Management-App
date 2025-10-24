429066cdb1040629bfc66815f01a3dfc2f3d4f49
Shweta Nigam <shwetanigam2106@gmail.com>
2025-10-24 18:46:21 +0530

    style: use prettier in main.txs file

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,16 +4,15 @@ import "./index.css";
 import App from "./App.tsx";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { AuthProvider } from "./context/AuthContext.tsx";
-// import { BrowserRouter } from "react-router-dom";
 
 const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")!).render(
-  <StrictMode>                                                  
-    <QueryClientProvider client={queryClient}>   
-    <AuthProvider>
+  <StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <AuthProvider>
         <App />
-        </AuthProvider>
+      </AuthProvider>
     </QueryClientProvider>
   </StrictMode>
 );
